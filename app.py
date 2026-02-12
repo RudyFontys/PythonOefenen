f437356693bf94216496ec76cc22557a29a4fe08
RudyFontys <r.bouland@fontys.nl>
2026-02-12 14:20:58 +0100

    Mijn oefenen tot les 3

---
+++ app.py
@@ -0,0 +1,23 @@
+# print("Running script.")
+# wie = 'Rudy de IT god'
+# print("'wie' is now equal to", wie)
+
+# print("kerkstraat", 3, end=" ") #end="" plakt de onderstaande print op dezelfde regel
+# print("en", "kerkstraat", 5)
+
+# lengte = 5
+# breedte = 2
+# opperplakte = lengte * breedte
+# print ("de oppervlakte is", lengte, "x", breedte, "=", opperplakte)
+
+# variabele = input("type hier iets: ") 
+# print(variabele)
+
+# tekstlengte = len("RudyIs1m82")
+# print(tekstlengte) 
+# print( "Debils"[2:5] ) 
+
+origineel = 'Rudy de IT god uit Tilburg'
+lower_case = origineel.lower() 
+upper_case = origineel.upper() 
+print(lower_case, upper_case)
